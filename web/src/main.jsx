708455d10618831dcff028d09c9a06b057486049
ussumane <ussumane.momade@uem.ac.mz>
2024-09-07 00:44:22 +0200

    update main.jsx

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Login from './pages/Login/Login.jsx'
+import Signup from './pages/Signup/Signup.jsx'
+import ShortlinkAnonimous from './pages/ShortlinkAnonimous/ShortlinkAnonimous.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* <Login /> */}
+    <ShortlinkAnonimous />
+    {/* <Signup /> */}
   </StrictMode>,
 )
