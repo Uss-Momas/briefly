00843916da54acae3813f148142bcd0e23e4514a
ussumane <ussumane.momade@uem.ac.mz>
2024-09-06 21:01:24 +0200

    minimalistic spotify clone with tailwind css

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,5 +1,5 @@
-import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight } from 'lucide-react';
-// import logo from './in'
+import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat, Mic2, LayoutList, Laptop2, Volume, Maximize, Maximize2 } from 'lucide-react';
+import waistingAlbumPhoto from './wasting-light.jpg';
 
 function App() {
 
@@ -38,38 +38,127 @@ function App() {
           <main className='flex-1 p-6'>
             <div className='flex items-center gap-4'>
               <button className='rounded-full bg-black/40 p-1'>
-                < ChevronLeft/>
+                < ChevronLeft />
               </button>
               <button className='rounded-full bg-black/40 p-1'>
-                < ChevronRight/>
+                < ChevronRight />
               </button>
             </div>
             <h1 className='font-semibold text-3xl mt-10'>Good Afternoon</h1>
 
-            <div className='grid grid-cols-3 gap-4'>
-              <div className='bg-white/10 rounded'>
-                <img src="" alt="" />
-              </div>
-              <div className='bg-white/10 rounded'>
-                <img src="" alt="" />
-              </div>
-              <div className='bg-white/10 rounded'>
-                <img src="" alt="" />
-              </div>
-              <div className='bg-white/10 rounded'>
-                <img src="" alt="" />
-              </div>
-              <div className='bg-white/10 rounded'>
-                <img src="" alt="" />
-              </div>
-              <div className='bg-white/10 rounded'>
-                <img src="" alt="" />
-              </div>
+            <div className='grid grid-cols-3 gap-4 mt-4'>
+              <a href='' className='bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+                <img className='w-28 h-28' src={waistingAlbumPhoto} alt="capa do album waisting light da banda fullfighters" />
+                <strong>Waisting Light</strong>
+                <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                  < Play />
+                </button>
+              </a>
+              <a href='' className='bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+                <img className='w-28 h-28' src={waistingAlbumPhoto} alt="capa do album waisting light da banda fullfighters" />
+                <strong>Waisting Light</strong>
+                <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                  < Play />
+                </button>
+              </a>
+              <a href='' className='bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+                <img className='w-28 h-28' src={waistingAlbumPhoto} alt="capa do album waisting light da banda fullfighters" />
+                <strong>Waisting Light</strong>
+                <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                  < Play />
+                </button>
+              </a>
+              <a href='' className='bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+                <img className='w-28 h-28' src={waistingAlbumPhoto} alt="capa do album waisting light da banda fullfighters" />
+                <strong>Waisting Light</strong>
+                <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                  < Play />
+                </button>
+              </a>
+              <a href='' className='bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+                <img className='w-28 h-28' src={waistingAlbumPhoto} alt="capa do album waisting light da banda fullfighters" />
+                <strong>Waisting Light</strong>
+                <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                  < Play />
+                </button>
+              </a>
+              <a href='' className='bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+                <img className='w-28 h-28' src={waistingAlbumPhoto} alt="capa do album waisting light da banda fullfighters" />
+                <strong>Waisting Light</strong>
+                <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                  < Play />
+                </button>
+              </a>
+            </div>
+
+            <h2 className='font-semibold text-2xl mt-10'>Made for Ussumane Momade</h2>
+            <div className='grid grid-cols-8 gap-4 mt-4'>
+              <a href='' className='bg-white/5 p-3 rounded flex flex-col gap-2 overflow-hidden hover:bg-white/10'>
+                <img className='w-full rounded' src={waistingAlbumPhoto} alt="capa do album waisting light da banda fullfighters" />
+                <strong className='font-semibold'>Daily Mix 1</strong>
+                <span className='text-sm text-zinc-500'>Wallows, Coin, girl in red and more</span>
+              </a>
+              <a href='' className='bg-white/5 p-3 rounded flex flex-col gap-2 overflow-hidden hover:bg-white/10'>
+                <img className='w-full rounded' src={waistingAlbumPhoto} alt="capa do album waisting light da banda fullfighters" />
+                <strong className='font-semibold'>Daily Mix 1</strong>
+                <span className='text-sm text-zinc-500'>Wallows, Coin, girl in red and more</span>
+              </a>
+              <a href='' className='bg-white/5 p-3 rounded flex flex-col gap-2 overflow-hidden hover:bg-white/10'>
+                <img className='w-full rounded' src={waistingAlbumPhoto} alt="capa do album waisting light da banda fullfighters" />
+                <strong className='font-semibold'>Daily Mix 1</strong>
+                <span className='text-sm text-zinc-500'>Wallows, Coin, girl in red and more</span>
+              </a>
+              <a href='' className='bg-white/5 p-3 rounded flex flex-col gap-2 overflow-hidden hover:bg-white/10'>
+                <img className='w-full rounded' src={waistingAlbumPhoto} alt="capa do album waisting light da banda fullfighters" />
+                <strong className='font-semibold'>Daily Mix 1</strong>
+                <span className='text-sm text-zinc-500'>Wallows, Coin, girl in red and more</span>
+              </a>
+              <a href='' className='bg-white/5 p-3 rounded flex flex-col gap-2 overflow-hidden hover:bg-white/10'>
+                <img className='w-full rounded' src={waistingAlbumPhoto} alt="capa do album waisting light da banda fullfighters" />
+                <strong className='font-semibold'>Daily Mix 1</strong>
+                <span className='text-sm text-zinc-500'>Wallows, Coin, girl in red and more</span>
+              </a>
             </div>
           </main>
         </div>
-        <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
-          footer
+        <footer className="bg-zinc-800 border-t border-zinc-700 p-6 flex items-center justify-between">
+          <div className='flex items-center gap-3'>
+            <img className='w-12 h-w-12 rounded' src={waistingAlbumPhoto} alt="capa do album waisting light da banda fullfighters" />
+            <div className='flex flex-col'>
+              <strong className='font-normal'>Rope</strong>
+              <span className='text-xs text-zinc-400'>Foo Fighters</span>
+            </div>
+          </div>
+          <div className='flex flex-col items-center gap-2'>
+            <div className='flex items-center gap-4'>
+              < Shuffle size={20} className='text-zinc-200' />
+              < SkipBack size={20} className='text-zinc-200' />
+              <button className='w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-black'>
+                < Play />
+              </button>
+              < SkipForward size={20} className='text-zinc-200' />
+              < Repeat size={20} className='text-zinc-200' />
+            </div>
+            <div className='flex items-center gap-2'>
+              <span className='texxt-xs text-zinc-500'>0:31</span>
+              <div className='h-1 rounded-full w-96 bg-zinc-600'>
+                <div className='bg-zinc-200 w-40 h-1 rounded-full'></div>
+              </div>
+              <span className='texxt-xs text-zinc-500'>2:41</span>
+            </div>
+          </div>
+          <div className='flex items-center gap-2'>
+            < Mic2 size={20} />
+            < LayoutList size={20} />
+            < Laptop2 size={20} />
+            <div className='flex items-center gap-2'>
+              < Volume size={20} />
+              <div className='h-1 rounded-full w-24 bg-zinc-600'>
+                <div className='bg-zinc-200 w-10 h-1 rounded-full'></div>
+              </div>
+            </div>
+            < Maximize2 size={20}/>
+          </div>
         </footer>
       </div>
     </>
