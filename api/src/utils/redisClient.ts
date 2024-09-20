2c2e56d5ef88d2350b2992a512414f41cabd53db
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2024-09-20 12:17:11 +0200

    handling redis connect errors

--- api/src/utils/redisClient.ts
+++ api/src/utils/redisClient.ts
@@ -2,4 +2,6 @@ import { createClient } from 'redis';
 
 export const redisClient = createClient({ url: 'redis://:@localhost:6379' });
 
-redisClient.connect();
+redisClient.connect().catch((e) => {
+    console.log("Couldnt connected", e.message);
+});
