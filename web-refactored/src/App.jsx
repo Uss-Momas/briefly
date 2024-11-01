746f34bf8d239ba5f95552f2d021dfc791ed1e65
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2024-11-02 01:40:33 +0200

    setting up the dashboard-home for user

--- web-refactored/src/App.jsx
+++ web-refactored/src/App.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Dashboard from "./pages/Auth/Dashboard";
 
 const router = createBrowserRouter([
   {
@@ -16,6 +17,10 @@ const router = createBrowserRouter([
     path: '/signup',
     element: <Signup />,
   },
+  {
+    path: '/auth/dashboard',
+    element: <Dashboard />,
+  }
 ]);
 
 export default function App() {
