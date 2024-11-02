0832a8ee4ddcece180a27c6a474f8a1378bdecac
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2024-11-02 17:14:01 +0200

    creatint the Header and Footer Components

---
+++ web-refactored/src/components/UserHeader/index.jsx
@@ -0,0 +1,69 @@
+import { AlignJustify, ChartNoAxesColumnIcon, LucideLink, User, X } from "lucide-react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function UserHeader() {
+    const [mobileNav, setMobileNav] = useState(false);
+
+    const handleMenuClick = (event) => {
+        event.preventDefault();
+        setMobileNav(!mobileNav);
+    }
+
+    return (
+        <header className="fixed w-full h-16 z-50 bg-white shadow-md">
+            <nav>
+                <div className="flex items-center justify-between p-2 sm:px-16 sm:py-4">
+                    <Link to={"/"}>
+                        <span className="text-purple-700 text-2xl font-bold" >Briefly</span>
+                    </Link>
+                    <ul className="flex gap-3 items-center max-sm:hidden transition ease-in-out">
+                        <li>
+                            <Link to={"/auth/dashboard"} className="flex items-center text-gray-600 text-base font-medium hover:text-purple-800 hover:bg-gray-100 rounded-lg px-3 py-2">
+                                <LucideLink className="h-6 w-6 mr-2" />
+                                <p>Links</p>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={"/auth/analytics"} className="flex items-center text-gray-600 text-base font-medium hover:text-purple-800 hover:bg-gray-100 rounded-lg px-3 py-2">
+                                <ChartNoAxesColumnIcon className="h-6 w-6 mr-2" />
+                                <p>Analytics</p>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={"/auth/profile"} className="flex items-center text-gray-600 text-base font-medium hover:text-purple-800 hover:bg-gray-100 rounded-lg px-3 py-2">
+                                <User className="h-6 w-6 mr-2" />
+                                <p>Profile</p>
+                            </Link>
+                        </li>
+                    </ul>
+                    <button onClick={handleMenuClick} className="sm:hidden text-zinc-600 border-2 border-white  p-2 hover:text-purple-600 hover:bg-zinc-200 hover:rounded-lg hover:border-2 hover:border-purple-600" >
+                        {mobileNav ? <X /> : <AlignJustify />}
+                    </button>
+                </div>
+                {mobileNav && (
+                    <ul className="sm:hidden flex flex-col gap-2 absolute bg-white w-full">
+                        <li>
+                            <Link to={"/auth/dashboard"} className="flex justify-center items-center text-gray-600 text-base font-medium hover:text-purple-800 hover:bg-gray-100 rounded-lg px-3 py-2">
+                                <LucideLink className="h-6 w-6 mr-2" />
+                                <p>Links</p>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={"/auth/analytics"} className="flex justify-center items-center text-gray-600 text-base font-medium hover:text-purple-800 hover:bg-gray-100 rounded-lg px-3 py-2">
+                                <ChartNoAxesColumnIcon className="h-6 w-6 mr-2" />
+                                <p>Analytics</p>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={"/auth/profile"} className="flex justify-center items-center text-gray-600 text-base font-medium hover:text-purple-800 hover:bg-gray-100 rounded-lg px-3 py-2">
+                                <User className="h-6 w-6 mr-2" />
+                                <p>Profile</p>
+                            </Link>
+                        </li>
+                    </ul>
+                )}
+            </nav>
+        </header>
+    );
+}
--- web-refactored/src/pages/Auth/Dashboard/index.jsx
+++ web-refactored/src/pages/Auth/Dashboard/index.jsx
@@ -1,71 +1,12 @@
-import { AlignJustify, ChartNoAxesColumnIcon, CheckCircle, Copy, ExternalLink, Linkedin, LinkIcon, LucideLink, Mail, MessageCircle, Trash2, Twitter, User, X } from "lucide-react";
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Copy, ExternalLink, LinkIcon, Trash2, } from "lucide-react";
+import Footer from "../../../components/Footer";
+import UserHeader from "../../../components/UserHeader";
 
 export default function Dashboard() {
-    const [mobileNav, setMobileNav] = useState(false);
-
-    const handleMenuClick = (event) => {
-        event.preventDefault();
-        setMobileNav(!mobileNav);
-    }
 
     return (
         <div className="min-h-dvh bg-gradient-to-r from-zircon-50 via-zircon-100 to-zircon-50">
-            <header className="fixed w-full h-16 z-50 bg-white shadow-md">
-                <nav>
-                    <div className="flex items-center justify-between p-2 sm:px-16 sm:py-4">
-                        <Link to={"/"}>
-                            <span className="text-purple-700 text-2xl font-bold" >Briefly</span>
-                        </Link>
-                        <ul className="flex gap-3 items-center max-sm:hidden transition ease-in-out">
-                            <li>
-                                <Link to={"/auth/dashboard"} className="flex items-center text-gray-600 text-base font-medium hover:text-purple-800 hover:bg-gray-100 rounded-lg px-3 py-2">
-                                    <LucideLink className="h-6 w-6 mr-2" />
-                                    <p>Links</p>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to={"/auth/analytics"} className="flex items-center text-gray-600 text-base font-medium hover:text-purple-800 hover:bg-gray-100 rounded-lg px-3 py-2">
-                                    <ChartNoAxesColumnIcon className="h-6 w-6 mr-2" />
-                                    <p>Analytics</p>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to={"/auth/profile"} className="flex items-center text-gray-600 text-base font-medium hover:text-purple-800 hover:bg-gray-100 rounded-lg px-3 py-2">
-                                    <User className="h-6 w-6 mr-2" />
-                                    <p>Profile</p>
-                                </Link>
-                            </li>
-                        </ul>
-                        <button onClick={handleMenuClick} className="sm:hidden text-zinc-600 border-2 border-white  p-2 hover:text-purple-600 hover:bg-zinc-200 hover:rounded-lg hover:border-2 hover:border-purple-600" >
-                            {mobileNav ? <X /> : <AlignJustify />}
-                        </button>
-                    </div>
-                    {mobileNav && (
-                        <ul className="sm:hidden flex flex-col gap-2 absolute bg-white w-full">
-                            <li>
-                                <Link to={"/auth/dashboard"} className="flex justify-center items-center text-gray-600 text-base font-medium hover:text-purple-800 hover:bg-gray-100 rounded-lg px-3 py-2">
-                                    <LucideLink className="h-6 w-6 mr-2" />
-                                    <p>Links</p>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to={"/auth/analytics"} className="flex justify-center items-center text-gray-600 text-base font-medium hover:text-purple-800 hover:bg-gray-100 rounded-lg px-3 py-2">
-                                    <ChartNoAxesColumnIcon className="h-6 w-6 mr-2" />
-                                    <p>Analytics</p>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to={"/auth/profile"} className="flex justify-center items-center text-gray-600 text-base font-medium hover:text-purple-800 hover:bg-gray-100 rounded-lg px-3 py-2">
-                                    <User className="h-6 w-6 mr-2" />
-                                    <p>Profile</p>
-                                </Link>
-                            </li>
-                        </ul>
-                    )}
-                </nav>
-            </header>
+            <UserHeader />
             <main className="pt-16">
                 <section className="bg-white/30 border border-purple-800/15 backdrop-blur-md rounded-lg shadow-lg p-4 mx-auto my-6 max-w-5xl sm:p-6 lg:p-8">
                     <div className="flex flex-col gap-6 items-center ">
@@ -134,41 +75,7 @@ export default function Dashboard() {
                     </div>
                 </section>
             </main>
-            <footer className="bg-zinc-900/90 text-white py-12 px-6">
-                <div className="flex flex-col justify-center gap-6">
-                    <div className="flex flex-col gap-10 sm:flex-row">
-                        <section className="flex flex-col gap-4">
-                            <h1 className="text-xl font-semibold">Briefly</h1>
-                            <div className="flex gap-4 text-gray-500">
-                                <a href="https://google.com" target="_blank"><Linkedin className="hover:text-white" /></a>
-                                <a href="https://google.com" target="_blank"><Twitter className="hover:text-white" /></a>
-                                <a href="https://google.com" target="_blank"><MessageCircle className="hover:text-white" /></a>
-                            </div>
-                            <div className="flex gap-4 text-gray-500 hover:text-white">
-                                <Mail />
-                                <a href="mailto:contact@briefly.com"><p>contact@briefly.com</p></a>
-                            </div>
-                        </section>
-                        <section className="flex flex-col gap-4">
-                            <h2 className="text-lg font-medium">Quick Links</h2>
-                            <nav className="text-gray-500">
-                                <ul className="flex flex-col gap-4">
-                                    <li>
-                                        <a href="#about" className="hover:text-white">About Us</a>
-                                    </li>
-                                    <li>
-                                        <a href="#services" className="hover:text-white">Our Services</a>
-                                    </li>
-                                </ul>
-                            </nav>
-                        </section>
-                    </div>
-                    <div className="flex flex-col gap-4 text-gray-500 text-center">
-                        <hr className="border-gray-500" />
-                        <p>&copy; 2024 Briefly. All Rights Reserved</p>
-                    </div>
-                </div>
-            </footer>
+            <Footer />
         </div>
     );
 }
