5a6be5a03b353bb9b7e9ac85d7ef4b463d85e796
Ussumane Momade <89344441+Uss-Momas@users.noreply.github.com>
2024-11-08 13:36:51 +0200

    ignoring redis code if not conected

--- api/src/controllers/ShortlinkController.ts
+++ api/src/controllers/ShortlinkController.ts
@@ -11,7 +11,7 @@ class ShortlinkController {
         const { page = 1, limit = 5 } = paginationQuerySchema.parse(request.query);
         const user: any = request.user;
         const { shortlinks, meta } = await shortlinkRepository.getAllShortlinks({ page, limit, user });
-        
+
         return reply.send({ message: 'All Shortlinks', shortlinks, meta });
     }
 
@@ -29,8 +29,8 @@ class ShortlinkController {
 
         if (!shortlink) throw new AppError(404, "Original Link Was Not Found");
 
-        await redisClient.zIncrBy('metrics', 1, shortlink.id);
-        console.log(shortlink);
+        if (redisClient.isReady)
+            await redisClient.zIncrBy('metrics', 1, shortlink.id);
 
         return reply.send({ message: 'Get shortlink', originalUrl: shortlink.originalUrl });
     }
